b513d3e6f295026e5f6d8b62451d0dc049ac953b
craigmindset <irediacraig@gmail.com>
2026-01-27 09:54:00 +0100

    updated code

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ark of Light - Admin Panel",
+  title: "Ark of Light for all Nations - Admin Panel",
   description: "Admin panel for Ark of Light mobile app",
 };
 
